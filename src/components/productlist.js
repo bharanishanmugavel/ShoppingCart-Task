94dafb12ae6d8e2b85fb4b12c2d4900fc58436ad
Bharani <139566292+bharanishanmugavel@users.noreply.github.com>
2024-04-10 10:26:59 +0530

    Add files via upload

---
+++ src/components/productlist.js
@@ -0,0 +1,94 @@
+const navigationLinks = [
+  { text: "Home", url: "#" },
+  { text: "About", url: "#" },
+  {
+    text: "Shop",
+    url: "#",
+    subLinks: [
+      { text: "All Products", url: "#" },
+      { text: "Popular Items", url: "#" },
+      { text: "New Arrivals", url: "#" },
+    ],
+  },
+];
+
+const product_list = [
+  {
+    img: "https://m.media-amazon.com/images/I/317VTY6RngL._AC_UF894,1000_QL80_.jpg",
+    title: "MRF",
+    id:1.5,
+    rating:5,
+    newprice: 50,
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/618vqRe8+EL._AC_UF894,1000_QL80_.jpg",
+    title: "CEAT",
+    id:2,
+    rating:4,
+    sale: "Sale",
+    newprice: 45.0,
+  },
+  {
+    img: "https://belco.in/in/wp-content/uploads/magictoolbox_cache/2deb0600714fef155ed12324a046b1c5/2/8/2866/original/1388775440/17a.jpg",
+    title: "R-MAX",
+    id:3,
+    rating:3,
+    sale: "Sale",
+    newprice: 25.0,
+  },
+  {
+    img: "https://chhabrasports.com/wp-content/uploads/2020/04/APH_4664-scaled-4-600x600.jpg",
+    title: "SG",
+    id:4,
+    rating:3,
+    newprice: 42.0,
+  },
+  {
+    img: "https://cdn.shopify.com/s/files/1/0012/8797/9121/articles/Workshop_Shots-2470.jpg?v=1698059311",
+    title: "GRAY-NICOLS",
+    id:5,
+    rating:4,
+    sale: "Sale",
+    newprice: 40.0,
+  },
+  {
+    img: "https://sportsworld.com.bd/wp-content/uploads/2020/10/2.jpg",
+    title: "DSC",
+    id:6,
+    rating:2,
+    newprice: 25.0,
+  },
+  {
+    img: "https://gmcricket.in/media/catalog/product/cache/9bb9d677791f8666003e194c8a94aeff/g/m/gm-cricket-english-willow-bat-manna-333-1_.jpg",
+    title: "GM",
+    id:7,
+    rating:3,
+    sale: "Sale",
+    newprice: 30.0,
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/71sjinS04BL.jpg",
+    title: "SPARTAN",
+    id:8,
+    rating:4,
+    newprice: 35.0,
+  },
+  {
+    img: "https://www.tcscricket.com/wp-content/uploads/2023/10/REEBOK-PLAYERS-EDITION-PRO-ENGLISH-WILLOW-CRICKET-BAT-1.jpg",
+    title: "REEBOK",
+    id:9,
+    rating:3,
+    newprice: 35.0,
+  },
+  {
+    img: "https://westernsportscentre.com.au/cdn/shop/products/kookaburra-cricket-bats-kookaburra-kahuna-pro-players-cricket-bat-senior-37512646492410.jpg?v=1657378579&width=360",
+    title: "KOOKABURRA",
+    id:10,
+    rating:5,
+    sale: "Sale",
+    newprice: 43.0,
+  },
+  
+ 
+];
+export { product_list, navigationLinks };
